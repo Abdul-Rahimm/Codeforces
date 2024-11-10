3dbbdd30db082716812f131cf35a5dd74f86d919
Abdul-Rahimm <k213951@nu.edu.pk>
2024-11-11 00:31:12 +0500

    AC cses

---
+++ Increasing_Array.cpp
@@ -0,0 +1,57 @@
+// بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define int long long
+#define py cout << "YES\n";
+#define pn cout << "NO\n";
+#define forn(a, b) for (int i = a; i < b; i++)
+#define nl cout << endl;
+#define pii pair<int, int>
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+const int mod = 1e9 + 7;
+
+void solve();
+
+signed main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tc = 1;
+
+    while (tc--)
+        solve();
+
+    return 0;
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    vi nums(n);
+    for (int &i : nums)
+        cin >> i;
+
+    int ans = 0;
+    for (int i = 1; i < n; i++)
+    {
+        int curr = nums[i - 1];
+
+        if (nums[i] < curr)
+        {
+            ans += curr - nums[i];
+            nums[i] = curr;
+        }
+    }
+
+    cout << ans;
+}
---
+++ Permutations.cpp
@@ -0,0 +1,59 @@
+// بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define int long long
+#define py cout << "YES\n";
+#define pn cout << "NO\n";
+#define forn(a, b) for (int i = a; i < b; i++)
+#define nl cout << endl;
+#define pii pair<int, int>
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+const int mod = 1e9 + 7;
+
+void solve();
+
+signed main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tc = 1;
+
+    while (tc--)
+        solve();
+
+    return 0;
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+    if (n == 1)
+    {
+        cout << 1;
+        return;
+    }
+
+    if (n <= 3)
+    {
+        cout << "NO SOLUTION" << endl;
+        return;
+    }
+
+    for (int i = 2; i <= n; i += 2)
+    {
+        cout << i << " ";
+    }
+    for (int i = 1; i <= n; i += 2)
+    {
+        cout << i << " ";
+    }
+}
