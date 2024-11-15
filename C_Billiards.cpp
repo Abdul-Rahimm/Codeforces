7aa84556872774f9fc7b2ff21c895f7875bcda68
Abdul-Rahimm <k213951@nu.edu.pk>
2024-11-15 14:36:16 +0500

    AC

---
+++ C_Billiards.cpp
@@ -0,0 +1,44 @@
+// بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define int long long
+#define py cout << "YES\n";
+#define pn cout << "NO\n";
+#define forn(a, b) for (int i = a; i < b; i++)
+#define nl cout << endl;
+#define pii pair<int, int>
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+const int mod = 1e9 + 7;
+
+void solve();
+
+signed main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tc = 1;
+
+    while (tc--)
+        solve();
+
+    return 0;
+}
+
+void solve()
+{
+    double x1, y1, x2, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+
+    double ans = (x2 * y1) + (x1 * y2);
+    ans /= (y1 + y2);
+
+    cout << setprecision(11) << ans;
+}
