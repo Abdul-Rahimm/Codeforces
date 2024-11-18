b6836ab7add52a0b7aecc235cfde9a2d36e9c7c3
Abdul-Rahimm <k213951@nu.edu.pk>
2024-11-18 23:19:54 +0500

    graph DFS

--- Counting_Rooms.cpp
+++ Counting_Rooms.cpp
@@ -1,56 +1,73 @@
+// بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
 using namespace std;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define int long long
+#define py cout << "YES\n";
+#define pn cout << "NO\n";
+#define forn(a, b) for (int i = a; i < b; i++)
+#define nl cout << endl;
+#define pii pair<int, int>
 typedef long long ll;
-ll dx[] = {1, 0, -1, 0};
-ll dy[] = {0, 1, 0, -1};
-bool inrange(ll x, ll y, ll n, ll m)
-{
-    if (((0 <= x) && (x < n)) && ((0 <= y) && (y < m)))
-        return true;
-    return false;
-}
-int main()
-{
-    ll n, m;
-    cin >> n >> m;
-    vector<string> v(n);
-    vector<vector<ll>> vis(n, vector<ll>(m));
-    for (auto &i : v)
-    {
-        cin >> i;
-    }
-    ll cnt = 0;
-    queue<pair<ll, ll>> q;
-    for (ll i = 0; i < n; i++)
-    {
-        for (ll j = 0; j < m; j++)
-        {
-            if (v[i][j] == '#')
-                continue;
-            if (vis[i][j] == 1)
-                continue;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+const int mod = 1e9 + 7;
 
-            q.push({i, j});
-            vis[i][j] = 1;
-            cnt++;
-            while (q.size() > 0)
+void solve();
+int n, m;
+
+vii dir{{1, 0}, {0, 1}, {0, -1}, {-1, 0}};
+
+signed main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tc = 1;
+
+    while (tc--)
+        solve();
+
+    return 0;
+}
+void DFS(vector<string> &s, int i, int j)
+{
+    if (i < 0 || i >= n || j < 0 || j >= m || s[i][j] == '#')
+        return;
+
+    s[i][j] = '#'; // mark it visited
+
+    for (vector<int> row : dir)
+    {
+        int nx = i + row[0];
+        int ny = j + row[1];
+
+        DFS(s, nx, ny);
+    }
+}
+
+void solve()
+{
+    cin >> n >> m;
+
+    vector<string> s(n);
+    for (string &i : s)
+        cin >> i;
+
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+        {
+            if (s[i][j] == '.')
             {
-                ll x = q.front().first;
-                ll y = q.front().second;
-                q.pop();
-                for (ll j = 0; j < 4; j++)
-                {
-                    ll nx = x + dx[j];
-                    ll ny = y + dy[j];
-                    if ((inrange(nx, ny, n, m) && v[nx][ny] == '.') && vis[nx][ny] == 0)
-                    {
-                        vis[nx][ny] = 1;
-                        q.push({nx, ny});
-                    }
-                }
+                ans++;
+                DFS(s, i, j);
             }
         }
-    }
-    cout << cnt;
+
+    cout << ans;
 }
-// used simple bfs in a grid
