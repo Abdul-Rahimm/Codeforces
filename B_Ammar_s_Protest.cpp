b9aeb1fdaf0271a24ea18ec948c0b1dd3106db0d
Abdul-Rahimm <k213951@nu.edu.pk>
2025-01-15 22:32:34 +0500

    recent contest A and B. C and D couldnt do

---
+++ B_Ammar_s_Protest.cpp
@@ -0,0 +1,54 @@
+// بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define int long long
+#define py cout << "YES\n";
+#define pn cout << "NO\n";
+#define forn(a, b) for (int i = a; i < b; i++)
+#define nl cout << endl;
+#define pii pair<int, int>
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+const int mod = 1e9 + 7;
+
+void solve();
+
+signed main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tc = 1;
+
+    while (tc--)
+        solve();
+
+    return 0;
+}
+
+void solve()
+{
+    int n, x, y;
+    cin >> n >> x >> y;
+
+    int total = x;
+    int count = 1;
+
+    while (x > 0 and total * 2 < n)
+    {
+        x -= y;
+        total += x;
+        count++;
+    }
+
+    if (total * 2 >= n)
+        cout << count;
+    else
+        cout << "RIP";
+}
