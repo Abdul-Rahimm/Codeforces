bf58dac7dbb5a3f573ea08acf7ea08f1d2672273
Abdul-Rahimm <k213951@nu.edu.pk>
2025-02-05 15:05:37 +0500

    b was a sets problem

--- B_PROCOM_2025_VII.cpp
+++ B_PROCOM_2025_VII.cpp
@@ -19,7 +19,7 @@ typedef vector<vi> vii;
 const int mod = 1e9 + 7;
 
 void solve();
-
+// do this
 signed main(void)
 {
     ios::sync_with_stdio(false);
@@ -37,47 +37,16 @@ void solve()
     int n, a, b, c;
     cin >> n >> a >> b >> c;
 
-    set<int> st;
-    int count = n / a + n / b + n / c;
+    int counta = n / a;
+    int countb = n / b;
+    int countc = n / c;
 
-    int first = a * b;
-    int second = a * c;
-    int third = b * c;
-    int x = 0;
-    int y = 0;
-    int z = 0;
-    if (b % a == 0)
-    {
-        // then all multiples of b
-        x = b;
-    }
-    if (c % a == 0)
-    {
-        z = c;
-    }
+    // there will be some common multiples
+    int ab = n / lcm(a, b);
+    int bc = n / lcm(b, c);
+    int ac = n / lcm(a, c);
 
-    if (c % b == 0)
-    {
-        // then all multiples of b
-        y = c;
-    }
+    int all = n / lcm(a, lcm(b, c));
 
-    for (int i = 1; first * i < n or second * i < n or third * i < n or (x > 0 and x * i <= n) or (z > 0 and z * i <= n) or (y > 0 and y * i <= n); i++)
-    {
-        // cout << first * i << " " << second * i << " " << third * i << endl;
-        if (first * i <= n)
-            st.insert(first * i);
-        if (second * i <= n)
-            st.insert(second * i);
-        if (third * i <= n)
-            st.insert(third * i);
-        if (x * i <= n)
-            st.insert(x * i);
-        if (y * i <= n)
-            st.insert(y * i);
-        if (z * i <= n)
-            st.insert(z * i);
-    }
-
-    cout << count - st.size();
+    cout << counta + countb + countc - ab - ac - bc + all;
 }
