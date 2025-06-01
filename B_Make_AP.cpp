13e44ea7bb4eca3813c1257715b0ab075abad844
Abdul-Rahimm <k213951@nu.edu.pk>
2025-06-02 01:23:24 +0500

    900 rated

---
+++ B_Make_AP.cpp
@@ -0,0 +1,103 @@
+// بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define int long long
+#define py cout << "YES\n";
+#define pn cout << "NO\n";
+#define forn(a, b) for (int i = a; i < b; i++)
+#define nl cout << endl;
+#define pii pair<int, int>
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+const int mod = 1e9 + 7;
+
+void solve(int i);
+
+signed main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int tc = 1;
+    cin >> tc;
+
+    for (int i = 1; i <= tc; i++)
+        solve(i);
+
+    return 0;
+}
+
+void solve(int tc)
+{
+    // function<int(int, int)> solve = [&](int i, int j) -> int
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if (a - b == b - c)
+    {
+        py return;
+    }
+
+    // try for a
+    int cd = abs(b - c);
+    if (b > c)
+    {
+        // add cd to b
+        int newa = b + cd;
+        if (newa % a == 0)
+        {
+            py return;
+        }
+    }
+    else
+    {
+        // 1 10 15
+        int newa = b - cd;
+        if (newa % a == 0 and newa > 0)
+        {
+            py return;
+        }
+    }
+
+    // try for b
+    // b should be in bw a and c
+    if (a % 2 == c % 2)
+    {
+        int middle = (a + c) / 2;
+
+        if (middle % b == 0)
+        {
+            py return;
+        }
+    }
+
+    // try for c
+    cd = abs(b - a);
+    if (a < b)
+    {
+        // 10 20 3
+        int newc = b + cd;
+        if (newc % c == 0)
+        {
+            py return;
+        }
+    }
+    else
+    {
+        // 40 30 2
+        int newc = b - cd;
+        if (newc % c == 0 and newc > 0)
+        {
+            py return;
+        }
+    }
+
+    pn
+}
