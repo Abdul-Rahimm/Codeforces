d35a5cc8d3804e80edc2aa126ab94e98370fbbdf
Abdul-Rahimm <k213951@nu.edu.pk>
2025-06-05 16:10:30 +0500

    learned new to work with ceil

---
+++ A_Strange_Partition.cpp
@@ -0,0 +1,57 @@
+// بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define int long long
+#define py cout << "YES\n";
+#define pn cout << "NO\n";
+#define forn(a, b) for (int i = a; i < b; i++)
+#define nl cout << endl;
+#define pii pair<int, int>
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+const int mod = 1e9 + 7;
+
+void solve(int i);
+
+signed main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int tc = 1;
+    cin >> tc;
+
+    for (int i = 1; i <= tc; i++)
+        solve(i);
+
+    return 0;
+}
+
+void solve(int tc)
+{
+    // function<int(int, int)> solve = [&](int i, int j) -> int
+
+    int n, x;
+    cin >> n >> x;
+    int mx = 0;
+    int sum = 0;
+
+    vi nums(n);
+    for (int &i : nums)
+    {
+        cin >> i;
+
+        mx += ceil(i * 1.0 / x);
+        sum += i;
+    }
+    int mn = ceil(sum * 1.0 / x);
+
+    cout << mn << " " << mx << endl;
+}
