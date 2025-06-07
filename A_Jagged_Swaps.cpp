a8df52a0c956d72943d88a736dcfa4d1749bba3e
Abdul-Rahimm <k213951@nu.edu.pk>
2025-06-07 19:16:23 +0500

    5 800-rated questions

---
+++ A_Jagged_Swaps.cpp
@@ -0,0 +1,79 @@
+// بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define int long long
+#define py cout << "YES\n";
+#define pn cout << "NO\n";
+#define forn(a, b) for (int i = a; i < b; i++)
+#define nl cout << endl;
+#define pii pair<int, int>
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+const int mod = 1e9 + 7;
+
+void solve(int i);
+
+signed main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int tc = 1;
+    cin >> tc;
+
+    for (int i = 1; i <= tc; i++)
+        solve(i);
+
+    return 0;
+}
+void swap(int &i, int &j)
+{
+    int temp = i;
+    i = j;
+    j = temp;
+}
+void solve(int tc)
+{
+    // function<int(int, int)> solve = [&](int i, int j) -> int
+
+    int n;
+    cin >> n;
+
+    vi nums(n);
+    for (int &i : nums)
+        cin >> i;
+
+    if (nums[0] != 1)
+    {
+        pn return;
+    }
+
+    py return;
+}
+// forn(1, n - 1)
+// {
+//     int index = i + 1;
+
+//     if (nums[i] != index)
+//     {
+//         // wrong number at index
+//         if (nums[i - 1] + 2 == nums[i] and nums[i] - 1 == nums[i + 1])
+//         {
+//             swap(nums[i], nums[i + 1]);
+//         }
+//     }
+// }
+
+// if (is_sorted(nums.begin(), nums.end()))
+// {
+//     py return;
+// }
+// else
+//     pn
